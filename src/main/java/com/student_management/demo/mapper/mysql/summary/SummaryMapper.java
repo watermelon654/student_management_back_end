8d26a0d2027769e3ccfa8426ddb48ca567230e49
dumplant <2437789306@qq.com>
2023-05-20 18:20:14 +0800

    修改summary Controller

--- src/main/java/com/student_management/demo/mapper/mysql/summary/SummaryMapper.java
+++ src/main/java/com/student_management/demo/mapper/mysql/summary/SummaryMapper.java
@@ -2,43 +2,19 @@ package com.student_management.demo.mapper.mysql.summary;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.extension.toolkit.Db;
-import com.student_management.demo.mapper.dataobject.summary.SummaryDO;
-
-import java.util.Collection;
-
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.student_management.demo.controller.summary.vo.SummaryVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.toolkit.Db;
+import com.student_management.demo.mapper.dataobject.summary.SummaryDO;
+
 public interface SummaryMapper extends BaseMapper<SummaryDO> {
 
-    /**
-     * 按照学号查询Summary
-     * @param stu_num
-     * @return
-     */
-    default SummaryDO selectSummaryByStuNum(String stu_num) {
-        QueryWrapper<SummaryDO> wrapper = new QueryWrapper<>();
-        //查询条件
-        wrapper.eq("stu_num", stu_num);
-        return selectOne(wrapper);
-    }
-
-    /**
-     * 按照学生id查询Summary
-     * @param stu_id
-     * @return
-     */
-    default SummaryDO selectSummaryByStuId(Long stu_id) {
-        QueryWrapper<SummaryDO> wrapper = new QueryWrapper<>();
-        //查询条件
-        wrapper.eq("stu_id", stu_id);
-        return selectOne(wrapper);
-    }
     //    /**
 //     * 按照学号查询学生id
 //     * @param num
