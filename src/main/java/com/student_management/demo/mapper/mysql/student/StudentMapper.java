b4fdabad1fb9e8ebe0b08481bec64ff9665b0e4b
watermelon654 <131755928+watermelon654@users.noreply.github.com>
2023-05-20 10:55:04 +0800

    Update StudentMapper.java

--- src/main/java/com/student_management/demo/mapper/mysql/student/StudentMapper.java
+++ src/main/java/com/student_management/demo/mapper/mysql/student/StudentMapper.java
@@ -2,9 +2,13 @@ package com.student_management.demo.mapper.mysql.student;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.student_management.demo.controller.summary.vo.SummaryVO;
 import com.student_management.demo.mapper.dataobject.student.StudentDO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mapper
 @Repository
@@ -21,6 +25,76 @@ public interface StudentMapper extends BaseMapper<StudentDO> {
         return selectOne(wrapper);
     }
 
+
+
+
+    /**
+     * 查询所有学生
+     * @param num
+     * @return
+     */
+    default List<StudentDO> selectStudent() {
+        QueryWrapper<StudentDO> wrapper = new QueryWrapper<>();
+        //查询条件
+//        wrapper.eq("num", num);
+        return selectList(wrapper);
+    }
+
+    /**
+     * 查询所有学生
+     * @param num
+     * @return
+     */
+    default List<Long> selectStudentbystatus() {
+        List<Long> idList = new ArrayList<>();
+        QueryWrapper<StudentDO> wrapper = new QueryWrapper<>();
+        wrapper.eq("status","0");
+        //查询条件
+//        wrapper.eq("num", num);
+        List<StudentDO> studentDOS = selectList(wrapper);
+
+        if (studentDOS.size() != 0 ){
+
+            for (StudentDO studentDO : studentDOS) {
+                Long getid = studentDO.getId();
+                idList.add(getid);
+            }
+
+        }
+        return idList;
+
+    }
+
+
+    default List<Long> selectStudentbystatus1() {
+        List<Long> idList = new ArrayList<>();
+        QueryWrapper<StudentDO> wrapper = new QueryWrapper<>();
+        wrapper.eq("status","1");
+        //查询条件
+//        wrapper.eq("num", num);
+        List<StudentDO> studentDOS = selectList(wrapper);
+
+        if (studentDOS.size() != 0 ){
+
+            for (StudentDO studentDO : studentDOS) {
+                Long getid = studentDO.getId();
+                idList.add(getid);
+            }
+
+        }
+        return idList;
+
+    }
+
+    default int updateStu (StudentDO studentDO){
+        QueryWrapper<StudentDO> wrapper = new QueryWrapper<>();
+        wrapper.eq("id",studentDO.getId());
+        return update(studentDO,wrapper);
+    }
+
+
+
+
     /**
      * 插入学生基本信息用于生成学生id
      * @param name 姓名
@@ -28,6 +102,18 @@ public interface StudentMapper extends BaseMapper<StudentDO> {
      * @return
      */
     int insertStudentBasicInfo(String name,String num);
+
+
+
+
+
+    default void deleteByIds(List<Long> idList) {
+        deleteBatchIds(idList);
+    }
+
+
+
+
 
 
 }
